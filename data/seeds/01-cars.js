f3268ba0bb17296618356cc9692954f6280ae22e
abdullahi0520 <90243393+abdullahi0520@users.noreply.github.com>
2022-02-15 17:30:31 -0600

    second commit

--- data/seeds/01-cars.js
+++ data/seeds/01-cars.js
@@ -2,7 +2,7 @@
 const cars = [
   { 
        
-    vin: 1111111111111,
+    vin: 1111111111211,
     make: 'toyota',
     model: 'camry',
     mileage: 130000,
@@ -12,7 +12,7 @@ const cars = [
 },
 { 
        
-    vin: 1111111111111,
+    vin: 1111111131111,
     make: 'honda',
     model: 'civic',
     mileage: 140000,
@@ -22,7 +22,7 @@ const cars = [
 },
 { 
        
-    vin: 1111111111111,
+    vin: 1111111161111,
     make: 'nissan',
     model: 'sentra',
     mileage: 10000
@@ -32,7 +32,7 @@ const cars = [
 
 ];
 
-exports.seeds = function(knex) {
+exports.seed = function(knex) {
     return knex('cars').truncate().then(()=> {
         return knex('cars').insert(cars);
     });
