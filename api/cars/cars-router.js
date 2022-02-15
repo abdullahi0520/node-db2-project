902b457de2c13f1d0d7053b10d362dca7f9888a5
abdullahi0520 <90243393+abdullahi0520@users.noreply.github.com>
2022-02-15 17:50:08 -0600

    fleshed endpoints

--- api/cars/cars-router.js
+++ api/cars/cars-router.js
@@ -1,1 +1,23 @@
 // DO YOUR MAGIC
+const express = require('express');
+
+const router = express.Router()
+
+
+router.get('/', async (req,res, next) => {
+    res.json('getting all cars')
+})
+
+
+router.get('/:id', async (req,res, next) => {
+    res.json(`getting car wit id ${req.params.id}`)
+})
+
+
+router.post('/', async (req,res, next) => {
+    res.json('new car')
+})
+
+module.exports = router
+
+
