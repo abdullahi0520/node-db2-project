fe7656049295b4f2a9ce775204c9ba1c53f22f1f
abdullahi0520 <90243393+abdullahi0520@users.noreply.github.com>
2022-02-15 17:10:59 -0600

    migrations made

--- data/seeds/01-cars.js
+++ data/seeds/01-cars.js
@@ -1,1 +1,39 @@
 // STRETCH
+const cars = [
+  { 
+       
+    vin: 1111111111111,
+    make: 'toyota',
+    model: 'camry',
+    mileage: 130000,
+    title: 'clean',
+    transmission: 'automatic'
+
+},
+{ 
+       
+    vin: 1111111111111,
+    make: 'honda',
+    model: 'civic',
+    mileage: 140000,
+    title: 'clean'
+    
+
+},
+{ 
+       
+    vin: 1111111111111,
+    make: 'nissan',
+    model: 'sentra',
+    mileage: 10000
+
+
+}
+
+];
+
+exports.seeds = function(knex) {
+    return knex('cars').truncate().then(()=> {
+        return knex('cars').insert(cars);
+    });
+};
